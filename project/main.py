3a120a0a5433cc7c92edf1245512464b87a0ab7b
Mike Lay <mike@mkly.io>
2024-04-20 12:47:18 -0700

    Add update profile

--- project/main.py
+++ project/main.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
 from . import db
 
 main = Blueprint('main', __name__)
@@ -8,5 +9,15 @@ def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', user=current_user)
+
+@main.route('/profile', methods=['POST'])
+@login_required
+def profile_post():
+    current_user.name = request.form.get('name')
+    db.session.merge(current_user)
+    db.session.commit()
+    flash('Your name has been updated!')
+    return redirect(url_for('main.profile'))
