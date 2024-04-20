0cec5a5fc88dd66f17941098a9c47f34b7125904
Mike Lay <mike@mkly.io>
2024-04-20 13:15:25 -0700

    Add meetings

--- project/main.py
+++ project/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from .models import Meeting, User
 from . import db
 
 main = Blueprint('main', __name__)
@@ -21,3 +22,27 @@ def profile_post():
     db.session.commit()
     flash('Your name has been updated!')
     return redirect(url_for('main.profile'))
+
+@main.route('/meeting/<id>', methods=['GET'])
+@main.route('/meeting', methods=['GET'])
+@login_required
+def meeting(id=None):
+    meeting = Meeting().query.filter_by(id=id).first()
+    return render_template('meeting.html', meeting=meeting)
+
+@main.route('/meeting', methods=['POST'])
+@login_required
+def meeting_post():
+    title = request.form.get('title')
+    notes = request.form.get('notes')
+    user = User.query.filter_by(email=current_user.email).first()
+    user.created_meetings.append(Meeting(title=title, notes=notes))
+    db.session.commit()
+    flash('Meeting created successfully!')
+    return redirect(url_for('main.meetings'))
+
+@main.route('/meetings')
+@login_required
+def meetings():
+    user = User.query.filter_by(email=current_user.email).first()
+    return render_template('meetings.html', meetings=user.meetings)
--- project/models.py
+++ project/models.py
@@ -6,3 +6,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    meetings = db.relationship('Meeting', backref='user', lazy=True)
+    created_meetings = db.relationship('Meeting', backref='creator', lazy=True)
+
+class Meeting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    notes = db.Column(db.Text)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
